76ae29ba417c7d7a3dd9467787e4cb458e2fcdb0
Mani-mozhi <149560008+Mani-mozhi@users.noreply.github.com>
2024-01-12 18:36:19 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,20 +1,18 @@
-importjava.util.Scanner;
-public class reverseString
- {
-public static void main(String[] args)
- {
+import java.util.Scanner;
+public class stringReverse
+{
+public static void main(String[] args) 
+{
 System.out.println("PROGRAM TO REVERSE A STRING USING RECURSION");
 System.out.println("ENTER THE STRING TO BE REVERSED USING RECURSION");
 Scanner input = new Scanner(System.in);
 String str = input.nextLine();
-System.out.println("THE REVERSED STRING IS: " + reverseString(str));
+String reversed="";
+System.out.println("THE REVERSED STRING IS: " +reverseString(str,"",0,str.length()-1) );
 }
-public static String reverseString(Stringstr)
- {
-{
-if(str.isEmpty())
-returnstr;
-returnreverseString(str.substring(1))+str.charAt(0);
-}
-}
+
+public static String reverseString() {
+        
+        
+    }
 }
